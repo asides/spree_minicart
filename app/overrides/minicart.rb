d6767e61ee7404d99fcf824cbb994c7361f6f8c5
asides <yaroslav.str@gmail.com>
2013-11-09 14:28:02 +0200

    adding :original to override

--- app/overrides/minicart.rb
+++ app/overrides/minicart.rb
@@ -1,4 +1,5 @@
 Deface::Override.new(:virtual_path => 'spree/shared/_main_nav_bar',
                     :name => 'add_mini_cart',
                     :insert_bottom => "#link-to-cart",
-                    :partial => "spree/shared/minicart")
+                    :partial => "spree/shared/minicart",
+		    :original => '06e5d62e4ab532a1091e5ade9289f403e3c4a3b3')
